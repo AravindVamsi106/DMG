c3fec19381921321666419ca5edcc849040a244e
Aravind <110464587+AravindVamsi106@users.noreply.github.com>
2024-05-03 14:23:06 +0530

    Add files via upload

---
+++ Admin/dashboard/auth/sample.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Example code for php</title>
+</head>
+<body>
+	<?php
+		echo ~;
+		$oco =2;
+		echo $oco;
+	?>
+</body>
+</html>
